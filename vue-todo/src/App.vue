744e9a5bcd68eced44760355005b22f71ccbc120
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-11 23:58:43 +0900

    인풋 컴포넌트 생성 및 등록

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
+    <TodoInput></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoInput from "@/components/TodoInput.vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { TodoInput },
+});
 </script>
 
 <style scoped></style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    addTodo() {
+      console.log("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
