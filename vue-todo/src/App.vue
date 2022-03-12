de3cccf6fdf1faea63ddde37fb8a6a2daa0b36ed
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-12 09:42:39 +0900

    할 일 아이템 목록 컴포넌트 설계 및 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <h1>Vue Todo with Typescript</h1>
-    <TodoInput
-      :item="todoText"
-      @input="updateTodoText"
-      @add="addTodoItem"
-    ></TodoInput>
-    <!-- v-model 문법을 적용하려면 @input과 :value로 정의하셔야 합니다. :) -->
-    <!-- <TodoInput v-model="todoText" @add="addTodoItem"></TodoInput> -->
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <!-- v-model 문법을 적용하려면 @input과 :value로 정의하셔야 합니다. :) -->
+      <!-- <TodoInput v-model="todoText" @add="addTodoItem"></TodoInput> -->
+      <div>
+        <ul>
+          <TodoListItem></TodoListItem>
+          <!-- <li>아이템 1</li> -->
+          <!-- <li>아이템 2</li> -->
+          <!-- <li>아이템 3</li> -->
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "@/components/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoListItem, TodoInput },
   data() {
     return {
       todoText: "",
---
+++ vue-todo/src/components/TodoListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>아이템 1</li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
